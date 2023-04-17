87fdd8a14c0933875b62d0af43c8ebbcccff6c69
aufcoder <129998732+aufcoder@users.noreply.github.com>
2023-04-17 23:18:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+using Main;
+[TestFixture]
+public class MyTestClass
+{
+    [Test]
+    public void MyTestMethod()
+    {
+        Assert.IsTrue(Main.bar.Palindrome("racecar"));
+        Assert.IsFalse(Main.bar.Palindrome("Hello"));
+    }
+}
