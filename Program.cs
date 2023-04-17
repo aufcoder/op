1539366706eebd52d6a6e85c16a7fa4190c3934d
aufcoder <129998732+aufcoder@users.noreply.github.com>
2023-04-17 23:20:23 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Main
+{
+    public static class bar
+    {
+        public static void Main(string[] args) { }
+        public static bool Palindrome(string word)
+        {
+         
+            string reversed = new string(word.Reverse().ToArray());
+            bool IsPalindrome= word.Equals(reversed);
+
+            if (IsPalindrome)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nYes, Miku");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nNo, Miku");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            return IsPalindrome;
+        }
+    }
+}
+    
+
+
