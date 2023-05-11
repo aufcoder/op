485bcf4fcee95e5730ca8811eb55af4fe1d10d31
aufcoder <129998732+aufcoder@users.noreply.github.com>
2023-05-11 03:34:35 +0300

    Add files via upload

---
+++ folder1version/WebApplication1/Data/ContactsAPIContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class ContactsAPIContext : DbContext
+    {
+        public ContactsAPIContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet <Contact> Contacts { get; set; }
+    }
+}
