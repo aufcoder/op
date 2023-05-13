9abc3e10247c7faab23dc64ca9de3c24d895269c
aufcoder <129998732+aufcoder@users.noreply.github.com>
2023-05-13 03:51:49 +0300

    Add files via upload

---
+++ folder1version/WebApplication1.1/Models/Contact.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1._1.Models
+{
+    public class Contact
+    {
+        public Guid id { get; set; }
+        public string Fullname { get; set; }
+        public string Email { get; set; }
+        public long Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
